40021a1c8c35f9c80cdf771b4a1dc37cd67e14bb
Makova Nataliya <116758787+Crot2050@users.noreply.github.com>
2023-02-22 22:38:43 +0300

    Create 16.9.2.py

---
+++ 16.9.2.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    def __init__(self, x, y, width, heigth):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.heigth = heigth
+
+    def __str__(self):
+        return f'Rectangle : {self.x}, {self.y}, {self.width}, {self.heigth}.'
+
+    def area_rectangle(self):
+        return self.width * self.heigth
+
+
+rect_1 = Rectangle(5, 10, 50, 100)
+print(rect_1)
+print(f'Площадь прямоугольника: {rect_1.area_rectangle()}')
